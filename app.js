2d90afe9ea683a2dc197bb14e50c2481192a7c15
cristian <brexhe@hotmail.com>
2020-02-20 08:54:37 -0600

    Primera Version

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+//add the router
+app.use(express.static(__dirname + '/public'));
+
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Corriendo puerto 8080');
